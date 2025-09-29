fe3a2ecf4b0cadb4431b288327c43a41f7771522
Imam <imamzuhdimuzakkiy1@gmail.com>
2025-09-29 08:59:12 +0700

    Implement CRUD functionality for places and add method-override middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
@@ -19,6 +20,11 @@ mongoose.connect('mongodb://127.0.0.1/bestpoints')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+//routes
 app.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
 });
@@ -28,10 +34,37 @@ app.get('/places', async (req, res) => {
     res.render('places/index', { places });
 });
 
+app.get('/places/create', (req, res) => {
+    res.render('places/create');
+});
+
+app.post('/places', async (req, res) => {
+    const place = new Place(req.body.place);
+    await place.save();
+    res.redirect(`/places/${place._id}`);
+});
+
 app.get('/places/:id', async (req, res) => {
     const { id } = req.params;
     const place = await Place.findById(id);
     res.render('places/show', { place });
+});
+
+app.get('/places/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const place = await Place.findById(id);
+    res.render('places/edit', { place });
+});
+
+app.put('/places/:id', async (req, res) => {
+    await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
+    res.redirect('/places');
+});
+
+app.delete('/places/:id', async (req, res) => {
+    const { id } = req.params;
+    await Place.findByIdAndDelete(id);
+    res.redirect('/places');
 });
 
 // app.get('/seed/place', async (req, res) => {
