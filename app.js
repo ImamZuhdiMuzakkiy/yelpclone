912dce139b6e1fb23711229b387f36a6f024f8c2
Imam <imamzuhdimuzakkiy1@gmail.com>
2025-09-30 09:54:09 +0700

    Refactor place routes to use wrapAsync for error handling and add custom error handling middleware Enhance form validation in create and edit views for better user experience Implement ErrorHandler utility for consistent error management

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const ejsmate = require('ejs-mate');
 const express = require('express');
+const ErrorHandler = require('./utils/ErrorHandler');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const wrapAsync = require('./utils/wrapAsync');
 const path = require('path');
 const app = express();
 
@@ -31,54 +33,54 @@ app.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
 });
 
-app.get('/places', async (req, res) => {
+app.get('/places', wrapAsync(async (req, res) => {
     const places = await Place.find({});
     res.render('places/index', { places });
-});
+}));
 
-app.get('/places/create', (req, res) => {
+app.get('/places/create', wrapAsync(async (req, res) => {
     res.render('places/create');
-});
+}));
 
-app.post('/places', async (req, res) => {
+app.post('/places', wrapAsync(async (req, res) => {
     const place = new Place(req.body.place);
     await place.save();
     res.redirect(`/places/${place._id}`);
-});
+}));
 
-app.get('/places/:id', async (req, res) => {
+app.get('/places/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     const place = await Place.findById(id);
     res.render('places/show', { place });
-});
+}));
 
-app.get('/places/:id/edit', async (req, res) => {
+app.get('/places/:id/edit', wrapAsync(async (req, res) => {
     const { id } = req.params;
     const place = await Place.findById(id);
     res.render('places/edit', { place });
-});
+}));
 
-app.put('/places/:id', async (req, res) => {
+app.put('/places/:id', wrapAsync(async (req, res) => {
     await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
     res.redirect('/places');
-});
+}));
 
-app.delete('/places/:id', async (req, res) => {
+app.delete('/places/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Place.findByIdAndDelete(id);
     res.redirect('/places');
+}));
+
+// Cara 2 (lebih simpel, recommended)
+app.use((req, res, next) => {
+    next(new ErrorHandler('Page Not Found', 404));
 });
 
-// app.get('/seed/place', async (req, res) => {
-//     const place = new Place({
-//         title: 'Starbucks',
-//         price: '$$',
-//         description: 'A popular coffeehouse chain known for its specialty coffee drinks and cozy atmosphere.',
-//         location: '123 Coffee St, Seattle, WA'
-//     });
-//     await place.save();
-//     res.send(place);
-// });
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://127.0.0.1:3000');
